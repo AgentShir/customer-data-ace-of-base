07a5595ac91739dc71e4d593ae4fa33c4de33e7a
Shireen Garcia <garciasal@gmail.com>
2017-07-12 16:24:41 -0700

    Things finally started to show up on the page

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let container = document.querySelector('.container')
 
 let htmlString = `<article>`
+  htmlString += `<h2>Internal Company Directory</h2>`
   htmlString += `<name></name>`
   htmlString += `<email></email>`
   htmlString += `<location.street></location.street>`
