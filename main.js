02a3a10c3741eb8caa40269769761e928e1ba71c
Shireen Garcia <garciasal@gmail.com>
2017-07-12 19:03:50 -0700

    True story Jase broke my project but stayed late to fix it

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 let container = document.querySelector('.container')
 
-let htmlString = `<article>`
-  htmlString += `<h2>Internal Company Directory</h2>`
-  htmlString += `<name></name>`
-  htmlString += `<email></email>`
-  htmlString += `<location.street></location.street>`
-  htmlString += `<phone></phone>`
+let htmlString = ""
+    htmlString = `<div class=header>
+      <h1>Internal Company Directory</h1>
+    </div>
+    <div class="customerList">`
 
-
-htmlString += `</article>`
+customers.results.forEach(function(item){
+  htmlString +=
+  `<div class="customer">
+    <img src="${item.picture.large}">
+      <h2>${item.name.first} ${item.name.last}</h2>
+        <p>${item.email}</p>
+        <p>${item.location.street}</p>
+        <p>${item.location.city}, ${item.location.state} ${item.location.postcode}</p>
+        <p>${item.phone}</p>
+        <p>${item.id.value}</p>
+  </div>`
+})
+htmlString += `</div>`
 
 container.innerHTML= htmlString
